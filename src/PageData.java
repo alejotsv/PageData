62c2d4cd00757f644123c871195064a8a3d65bea
Alejandro Salas <asalas.wd@outlook.com>
2022-11-29 18:42:38 -0500

    Add initial variables

--- src/PageData.java
+++ src/PageData.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 public class PageData extends JFrame implements ActionListener, Runnable {
+    Runnable {
+
+        Thread runner;
+        String[] headers = { "Content-Length", "Content-Type", "Date", "Public", "Expires", "Last-Modified", "Server" };
+        URL page;
+        JTextField url;
+        JLabel[] headerLabel = new JLabel[7];
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
