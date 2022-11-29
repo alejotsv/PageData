e7743dfc73aba6a99299f7685994b6bcc82e0c6c
Alejandro Salas <asalas.wd@outlook.com>
2022-11-28 19:47:18 -0500

    Initial commit

---
+++ src/PageData.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PageData extends JFrame implements ActionListener, Runnable {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
