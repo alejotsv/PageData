be5b171a3d2d9f9fc84aceca9b0c70d07f5e8dc0
Alejandro Salas <asalas.wd@outlook.com>
2022-11-29 18:44:33 -0500

    Remove incorrect additional Runnable

--- src/PageData.java
+++ src/PageData.java
@@ -4,13 +4,17 @@ import java.awt.event.ActionListener;
 import java.net.URL;
 
 public class PageData extends JFrame implements ActionListener, Runnable {
-    Runnable {
 
         Thread runner;
         String[] headers = { "Content-Length", "Content-Type", "Date", "Public", "Expires", "Last-Modified", "Server" };
         URL page;
         JTextField url;
         JLabel[] headerLabel = new JLabel[7];
+        JTextField[] header = new JTextField[7];
+        JButton readPage, clearPage, quitLoading;
+        JLabel status;
+
+        public PageData
 
     }
 
